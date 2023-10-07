ef1fd8f133bc6980cffce20d6ec39ff6a71d982c
ksb <ksb9997@naver.com>
2023-10-07 10:51:59 +0900

    알고리즘 풀이 수정

--- Thisisthecodingtest_WorkBook/Chapter12_Implementation_Q08_322p.py
+++ Thisisthecodingtest_WorkBook/Chapter12_Implementation_Q08_322p.py
@@ -40,4 +40,4 @@ result.sort()
 if value != 0 :
     result.append(str(value))
 
-print('', join(result))
+print(''.join(result))
--- Thisisthecodingtest_WorkBook/Chapter12_Implementation_Q09_323p.py
+++ Thisisthecodingtest_WorkBook/Chapter12_Implementation_Q09_323p.py
@@ -51,7 +51,7 @@ def solution(s) :
         count = 1
         # 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
         for j in range(step, len(s), step) :
-        # 이전 상태와 동일하다면 아축 횟수(count) 증가
+        # 이전 상태와 동일하다면 압축 횟수(count) 증가
             if prev == s[j:j + step] :
                 count += 1
             # 다른 문자열이 나왔다면(더 이상 압축하지 못하는 경우라면)
