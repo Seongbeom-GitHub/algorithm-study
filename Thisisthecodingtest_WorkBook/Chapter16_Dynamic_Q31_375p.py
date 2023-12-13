83de1da0899b83ff357f1eaf92aadc3ec804c5c9
ksb <ksb9997@naver.com>
2023-12-13 13:37:54 +0900

    알고리즘 다이나믹 Q31 풀이 완료

---
+++ Thisisthecodingtest_WorkBook/Chapter16_Dynamic_Q31_375p.py
@@ -0,0 +1,44 @@
+# "이것이 코딩 테스트다" / 챕터16 / Dynamic Programming / 375p
+# Q31. 금광
+# 풀이일 : 2023-12-13
+# 난이도 : 1.5/3.0 
+# 시간 제한 : 1초
+# 메모리 제한 128MB
+# 풀이 제한 시간 : 30분
+# 소요 시간 : 
+# 문제 출처 : Flipkart 인터뷰
+
+for tc in range(int(input())) :
+    # 금광 정보 입력
+    n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+
+    # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
+    dp = []
+    index = 0
+    for i in range(n) :
+        dp.append(array[index : index + m])
+        index + m
+
+    # 다이나믹 프로그래밍 진행
+    for j in range(1, m) :
+        for i in range(n) :
+            # 왼쪽 위에서 오는 경우
+            if i == 0 :
+                left_up = 0
+            else :
+                left_up = dp[i -1][j - 1]
+            # 왼쪽 아래에서 오는 경우
+            if i == n - 1 :
+                left_down = 0
+            else :
+                left_down = dp[i + 1][j + 1]
+            # 왼쪽에서 오는 경우
+            left = dp[i][j - 1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+
+    result = 0
+    for i in range(n) :
+        result = max(result, dp[i][m - 1])
+
+    print(result)
