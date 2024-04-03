40dcf5acfc860aa7000eda4b814df3d71a7688ce
Seongbeom Kim <ksb9997@naver.com>
2024-04-03 20:31:43 +0900

    백준 1021번 풀이 완료 주석 추가

--- Baekjoon/1021.py
+++ Baekjoon/1021.py
@@ -1,9 +1,12 @@
 # 백준 1021번
 # 회전하는 큐 / 실버3
-
+# 풀이 : 답안 참고
+# 이해도 : 중간 (로직을 코드화 하는 과정에서 어려움이 있었음)
+# 소요 시간 : -
 
 from collections import deque
 import sys
+input = sys.stdin.readline
 
 # 큐의 크기, 뽑는 개수 입력
 m, n = map(int, input().split())
@@ -16,7 +19,7 @@ q = deque([i for i in range(1, m+1)])
 count = 0
 for target in targets :
         
-    while true :
+    while True :
         if target == q[0] :
             q.popleft()
             break
