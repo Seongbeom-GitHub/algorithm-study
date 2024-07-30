3a1bb7fb2e6b4cef0c7601a5ad9db4f7cadf8207
Seongbeom Kim <ksb9997@naver.com>
2024-07-30 22:35:57 +0900

    알고리즘 노트 소수 판별 코드 수정

--- AlgorithmNote/Math_isPrime.py
+++ AlgorithmNote/Math_isPrime.py
@@ -2,10 +2,11 @@
 
 import math
 
-def is_prime_number(x) :
+def isPrimeNumber(x):
+    # 1은 소수가 아니므로 False 반환
+    if x < 2:
+        return False
     for i in range(2, int(math.sqrt(x)) + 1) : # 2부터 x의 제곱근까지
-        if x % i == 0 :
+        if x % i == 0:
             return False
-        
     return True
-
