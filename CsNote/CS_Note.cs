080d8bcec768d6726159ffdd4cd0ffe5d4ef500e
Seongbeom Kim <ksb9997@naver.com>
2024-08-14 23:57:21 +0900

    C# 알고리즘 노트 추가

---
+++ CsNote/CS_Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MathUtils
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // 호출 예
+        int result = MathUtils.Add(3, 4); // 객체 생성 없이 호출 가능
+        Console.WriteLine(result); // 결과 출력
+    }
+}
