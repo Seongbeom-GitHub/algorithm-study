d11971b3408c6345c27d79b10960d33bf7e96ee9
Seongbeom Kim <ksb9997@naver.com>
2024-08-15 15:42:41 +0900

    cs 알고리즘 노트 수정

--- CsNote/CS_Note.cs
+++ CsNote/CS_Note.cs
@@ -1,29 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program {
 
+    static int n, m;
+    static List<int> array = new List<int>();
+
     static void Main() {
-        
-        int[] inputValue = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-        int n = inputValue[0];
-        int m = inputValue[1];
 
-        for(int i = 1; i < n + 1; i++) {
-            for(int j = 1; j < n + 1; j++) {
-                
-                if (i != j) {
-                    Console.WriteLine($"{i} {j}");
+        string[] inputValue = Console.ReadLine().Split(" ");
+        n = int.Parse(inputValue[0]);
+        m = int.Parse(inputValue[1]);
 
-                }
-
-
-            }
-        }
+        BackTracking();
 
     }
-    
-    
-    
-    
-    
+
+
+    // 재귀 호출 할 백트래킹 메서드
+    static void BackTracking() {
+        if (array.Count == m) {
+            Console.WriteLine(string.Join(" ", array));
+            return;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            if (!array.Contains(i)) { // i가 이미 선택되지 않았는지 체크
+                array.Add(i); // 배열에 i값 추가
+                BackTracking(); // 재귀 호출
+                array.RemoveAt(array.Count - 1); // 배열의 마지막 값 삭제
+            }
+        }
+    }
 }
--- CsNote/obj/Debug/net8.0/CsNote.AssemblyInfo.cs
+++ CsNote/obj/Debug/net8.0/CsNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d599981266b6388a45cc72c7a58b998d097e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b8cae50ed8aef56fa9feb5647317fa847e3e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
